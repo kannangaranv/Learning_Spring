9514e81a6794634118460286bf9dd77e2ea537eb
Nuwan Kannangara <kannangaranv@gmail.com>
2025-01-20 21:55:12 +0530

    GET POST PUT DELETE

--- SimpleWebApp/src/main/java/org/kannangaranv/simplewebapp/controller/ProductController.java
+++ SimpleWebApp/src/main/java/org/kannangaranv/simplewebapp/controller/ProductController.java
@@ -3,8 +3,7 @@ package org.kannangaranv.simplewebapp.controller;
 import org.kannangaranv.simplewebapp.model.Product;
 import org.kannangaranv.simplewebapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,28 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId ){
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        service.addProduct(prod);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
     }
 }
